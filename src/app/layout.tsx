66361097b7c2e3b2866ddea77a1141f81f3241ec
tomas <tomas_lx_99@live.com.pt>
2024-06-10 22:04:32 +0100

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/navbar";
 import Head from "next/head";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"], variable: '--font-roboto'});
-export const patrickHand = Patrick_Hand({
+const patrickHand = Patrick_Hand({
   subsets: ["latin"],
   weight: ["400"],
   variable: '--font-patrick-hand'
